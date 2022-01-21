6a651da78737ab0707cc774901b2d14d43f32469
Roland de Boer <roland@rolanddeboer.com>
2022-01-21 18:01:08 +0100

    .

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ int main( )
 //   ExcelWriter* excelWriter;
 //   string line;
 
-  inputParser = new InputParser();
+  //inputParser = new InputParser();
 
 //   // The compiler gives a warning about tmpnam. The reason why its use is discouraged is that even though the filename is unique at the time of calling tmpnam, it might not be unique any more by the time the file is actually created. Mkstemp is recommended because it actually creates the file. But because we are not actually in control of the file creation (Libxlxswriter is), we can't make use of that.
 //   char filename[L_tmpnam];
@@ -33,7 +33,7 @@ int main( )
 //   remove ( filename );
 
 //   delete excelWriter;
-   delete inputParser;
+   //delete inputParser;
 
    return 0;
 }
