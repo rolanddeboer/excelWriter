4dfc2b6fcb997bfbbed12b58808e7962d29fa147
Roland <roland@rolanddeboer.com>
2024-03-12 20:08:26 +0100

    .

--- ExcelWriter.cpp
+++ ExcelWriter.cpp
@@ -167,121 +167,120 @@ public:
     }
   }
 
+  void writeField(auto worksheet, Sheet &sheet, int row, int col) {
+    if ( 
+      sheet.columns[col].format == FORMAT_TEXT 
+      || sheet.columns[col].format == FORMAT_LONGTEXT 
+      || sheet.columns[col].format == FORMAT_BOOL 
+    ) {
+      worksheet_write_string( 
+        worksheet, 
+        row + 1, 
+        col, 
+        sheet.columns[col].stringValues[row].c_str(), 
+        NULL 
+      );
+    // Writing sum formula
+    } else if ( sheet.columns[col].sum > 0 ) {
+      double value = 0;
+      for (int i = 1; i <= sheet.columns[col].sum; i++) {
+        value += sheet.columns[col-i].intValues[row];
+      }
+      if ( sheet.columns[col].format == FORMAT_CURRENCY ) value = value / 100;
+      string formula = 
+        "=SUM(" 
+        + excelColFromNumber( col - sheet.columns[col].sum )
+        + to_string( row + 2 )
+        + ":" + excelColFromNumber( col - 1 )
+        + to_string( row + 2 )
+        + ")"
+      ;
+      worksheet_write_formula_num( 
+        worksheet, 
+        row + 1, 
+        col, 
+        formula.c_str(), 
+        NULL,
+        value
+      );
+    // Writing difference formula
+    } else if ( sheet.columns[col].diff ) {
+      string formula = 
+        "=" 
+        + excelColFromNumber( col - 2 )
+        + to_string( row + 2 )
+        + "-" + excelColFromNumber( col - 1 )
+        + to_string( row + 2 )
+      ;
+      double value = sheet.columns[col-2].intValues[row] - sheet.columns[col-1].intValues[row];
+      if ( sheet.columns[col].format == FORMAT_CURRENCY ) value = value / 100;
+      worksheet_write_formula_num( 
+        worksheet, 
+        row + 1, 
+        col, 
+        formula.c_str(), 
+        NULL,
+        value
+      );
+    // Writing number content
+    } else {
+      double value = sheet.columns[col].intValues[row];
+      if ( sheet.columns[col].showTotal ) sheet.columns[col].total += value;
+      if ( sheet.columns[col].format == FORMAT_CURRENCY ) value = value / 100;
+      if ( sheet.columns[col].format == FORMAT_DATE ) value = dateFromTimestamp( value );
+      if ( value > 0 ) {
+        worksheet_write_number( 
+          worksheet, 
+          row + 1, 
+          col, 
+          value, 
+          NULL 
+        );
+      }
+    }
+  }
+
+  void writeTotal(auto worksheet, Sheet &sheet, int row, int col) 
+  {
+    lxw_format *format = workbook_add_format (workbook );
+      format_set_bold( format );
+      format_set_font_name( format, text.fontname.c_str() );
+      format_set_font_size( format, text.fontsize );
+      if ( sheet.columns[col].format == FORMAT_CURRENCY ) {
+        sheet.columns[col].total = sheet.columns[col].total / 100;
+        format_set_num_format_index( format, 0x04 ); 
+      }
+
+      string colLetter = excelColFromNumber( col );
+      string formula = 
+        "=SUM(" 
+        + colLetter
+        + "2"
+        + ":" + colLetter
+        + to_string( row + 1 )
+        + ")"
+      ;
+      worksheet_write_formula_num( 
+        worksheet, 
+        row + 2, 
+        col, 
+        formula.c_str(), 
+        format,
+        sheet.columns[col].total
+      );
+  }
+
   void writeRows( auto worksheet, Sheet &sheet )
   {
-
     for (int col = 0; col < (int) sheet.columns.size(); col++) {
       int row;
       for (row = 0; row < sheet.rowCount; row++) {
-        // Writing text contet
-        if ( 
-          sheet.columns[col].format == FORMAT_TEXT 
-          || sheet.columns[col].format == FORMAT_LONGTEXT 
-          || sheet.columns[col].format == FORMAT_BOOL 
-        ) {
-          worksheet_write_string( 
-            worksheet, 
-            row + 1, 
-            col, 
-            sheet.columns[col].stringValues[row].c_str(), 
-            NULL 
-          );
-        // Writing sum formula
-        } else if ( sheet.columns[col].sum > 0 ) {
-          double value = 0;
-          for (int i = 1; i <= sheet.columns[col].sum; i++) {
-            value += sheet.columns[col-i].intValues[row];
-          }
-          if ( sheet.columns[col].format == FORMAT_CURRENCY ) value = value / 100;
-          string formula = 
-            "=SUM(" 
-            + excelColFromNumber( col - sheet.columns[col].sum )
-            + to_string( row + 2 )
-            + ":" + excelColFromNumber( col - 1 )
-            + to_string( row + 2 )
-            + ")"
-          ;
-          worksheet_write_formula_num( 
-            worksheet, 
-            row + 1, 
-            col, 
-            formula.c_str(), 
-            NULL,
-            value
-          );
-        // Writing difference formula
-        } else if ( sheet.columns[col].diff ) {
-          string formula = 
-            "=" 
-            + excelColFromNumber( col - 2 )
-            + to_string( row + 2 )
-            + "-" + excelColFromNumber( col - 1 )
-            + to_string( row + 2 )
-          ;
-          double value = sheet.columns[col-2].intValues[row] - sheet.columns[col-1].intValues[row];
-          if ( sheet.columns[col].format == FORMAT_CURRENCY ) value = value / 100;
-          worksheet_write_formula_num( 
-            worksheet, 
-            row + 1, 
-            col, 
-            formula.c_str(), 
-            NULL,
-            value
-          );
-        // Writing number content
-        } else {
-          double value = sheet.columns[col].intValues[row];
-          if ( sheet.columns[col].showTotal ) sheet.columns[col].total += value;
-          if ( sheet.columns[col].format == FORMAT_CURRENCY ) value = value / 100;
-          if ( sheet.columns[col].format == FORMAT_DATE ) value = dateFromTimestamp( value );
-          if ( value > 0 ) {
-            worksheet_write_number( 
-              worksheet, 
-              row + 1, 
-              col, 
-              value, 
-              NULL 
-            );
-          }
-        }
-      } // row loop
-      if ( sheet.columns[col].showTotal ) {
-        lxw_format *format = workbook_add_format (workbook );
-        format_set_bold( format );
-        format_set_font_name( format, text.fontname.c_str() );
-        format_set_font_size( format, text.fontsize );
-        if ( sheet.columns[col].format == FORMAT_CURRENCY ){
-          sheet.columns[col].total = sheet.columns[col].total / 100;
-          format_set_num_format_index( format, 0x04 ); 
-        }
-
-        string colLetter = excelColFromNumber( col );
-        string formula = 
-          "=SUM(" 
-          + colLetter
-          + "2"
-          + ":" + colLetter
-          + to_string( row + 1 )
-          + ")"
-        ;
-        worksheet_write_formula_num( 
-          worksheet, 
-          row + 2, 
-          col, 
-          formula.c_str(), 
-          format,
-          sheet.columns[col].total
-        );
-
-        // worksheet_write_number( 
-        //   worksheet, 
-        //   row + 2, 
-        //   col, 
-        //   sheet.columns[col].total, 
-        //   format 
-        // );
+        writeField( worksheet, sheet, row, col );
       }
-    } // col loop
+      if ( sheet.columns[col].showTotal ) {
+        writeTotal( worksheet, sheet, row, col );       
+      }
+    }
   }
 
 };
